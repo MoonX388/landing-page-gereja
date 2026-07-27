45c33a95b656e5d619264bba9757bdc3908d3dd8
MoonX388 <moonx388@gmail.com>
2026-07-27 19:10:02 +0700

    Delete app/dashboard/[id] directory

--- app/dashboard/[id]/domains/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/dashboard/[id]/domains/page.tsx
-
-import DomainsView from "../_views/DomainsView"
-
-export default function DomainsPage() {
-  // Kamu bisa melakukan Fetching data dari backend NestJS di sini jika dibutuhkan
-  
-  return <DomainsView />
-}
