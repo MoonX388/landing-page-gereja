41b5156e5c6488db647f8dd459b3e5d49f68c3f5
MoonX388 <moonx388@gmail.com>
2026-07-27 19:14:31 +0700

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // Ganti dengan URL API Anda 
+  baseURL: "https://api.gerejapintar.id", // Ganti dengan URL API Anda 
 });
 
 // Interceptor untuk menyisipkan Token JWT otomatis ke setiap request
